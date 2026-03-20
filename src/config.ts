0aaad432d4de7d626925d100327b57068f7346c4
arcwolf1 <16623722+arcwolf1@user.noreply.gitee.com>
2026-03-20 22:17:51 +0800

    fix:修复背景问题

--- src/config.ts
+++ src/config.ts
@@ -133,7 +133,7 @@ export const siteConfig: SiteConfig = {
 
 		// PicFlow API支持(智能图片API)
 		imageApi: {
-			enable: false, // 启用图片API
+			enable: true, // 启用图片API
 			url: "https://picflow.arcwolf.top/api_v2.php?format=text&count=5", // API地址，返回每行一个图片链接的文本
 		},
 		// 这里需要使用PicFlow API的Text返回类型,所以我们需要format=text参数
